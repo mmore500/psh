85c7aa8b3b38b7abc41765c004fb69f9d2fb9203
Matthew Moreno <matthew.andres.moreno@gmail.com>
2017-03-02 13:32:39 -0800

    Revise qotd test to handle missing qotd file
    The unit test now tests whether the qotd file is present or missing
    and then adjusts the expected outcome of the print_qotd function
    based on the result.

--- test/test_qotd.c
+++ test/test_qotd.c
@@ -4,7 +4,26 @@
 static bool test_print_qotd() {
   int pass = true;
 
-  pass &= (0 == print_qotd(1));
+  char *hPath = getenv("HOME");
+
+  // +1 for concatenating slash
+  int nChar = strlen(hPath) + strlen(QOTD_FILENAME) + 1;
+  char *fullPath = malloc(sizeof(char) * nChar);
+  // ensure that string starts empty
+  fullPath[0] = '\0';
+
+  // concatenate the path
+  strcat(fullPath, hPath);
+  strcat(fullPath, "/");
+  strcat(fullPath, QOTD_FILENAME);
+
+  if (fopen(fullPath, "r")) {
+    // if qotd file exists
+    pass &= (0 == print_qotd(1));
+  } else {
+    // if qotd file doesn't exist
+    pass &= (1 == print_qotd(1));
+  }
 
   return pass;
 }
