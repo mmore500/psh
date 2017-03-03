6e90a935c52319e26733623508a41bec6e7bbaae
Matthew Moreno <matthew.andres.moreno@gmail.com>
2017-03-02 20:19:49 -0800

    Implement main function

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 
 #include "builtin.h"
 #include "local.h"
@@ -16,8 +17,8 @@
 
 #define MAX_INPUT_LEN 256
 
-char buffer[MAX_INPUT_LEN];
-
+char buffer[HISTORY_LEN+1][MAX_INPUT_LEN];
+char *strarray[HISTORY_LEN+1];
 
 int main(int argc, char *argv[]) {
 
@@ -28,25 +29,69 @@ int main(int argc, char *argv[]) {
   // argument 0 makes it verbose
   print_qotd(0);
 
-  while(true) {
+  // initialize all history buffer to empty strings
+  // initialize an array of char pointers to point in there
+  int j;
+  for(j = 0; j < HISTORY_LEN + 1; j ++) {
+    buffer[j][0] = '\0';
+    strarray[j] = &buffer[j][0];
+  }
+
+  // buffer for path
+  long pbsize = pathconf(".", _PC_PATH_MAX);
+  char *pathbuff = (char *)malloc((size_t)pbsize);
+
+
+  int cur_hist;
+  for(cur_hist = 0; true; cur_hist = (cur_hist + 1) % (HISTORY_LEN+1)) {
+    // // clear input buffer
+    // fseek(stdin,0,SEEK_END);
+
     // get string data from user
-    printf("psh>");
+    printf("psh> ");
 
-    // clear input buffer
-    fseek(stdin,0,SEEK_END);
+    fgets(buffer[cur_hist], MAX_INPUT_LEN, stdin);
 
-    fgets(buffer, MAX_INPUT_LEN, stdin);
+    argbox *a = parse(buffer[cur_hist]);
 
-    //TODO check if & is last argument
+    // if no arguments then just show prompt again
+    if (a->count == 0) continue;
 
-    //TODO look for / prepended commands
-
-    //TODO look in the user PATH
-
-    // invalid command
-    printf("error: invalid command\n");
-
+    // look for trailing & on commands
+    bool wait = true;
+    if (!strcmp((a->words)[a->count-1],"&")) {
+      wait = false;
+      // remove & from args once it is parsed
+      (a->words)[a->count-1] = NULL;
     }
+
+    // see if it can be handled as a builtin
+    if (handle_builtin(a, (char **)strarray, false)) continue;
+
+    // path to exebutable to try
+    char *res = NULL;
+
+    // if prepended by a / try to execute raw
+    if ((a->words[0])[0] == '/') res = a->words[0];
+    // try to look in current directory
+    else if ((res = searchcwd(a->words[0], pathbuff, pbsize)))
+    ;
+    // try to look in bins on user's path
+    else if ((res = searchbins(a->words[0], pathbuff, pbsize)))
+    ;
+
+    // attempt to execute path
+    if(exec_path(res, a->words, wait, false)) {
+      printf("psh: invalid command\n");
+    }
+
+    // deallocate resources
+    destroy_argbox(a);
+
+  }
+
+  // deallocate resources
+  free(pathbuff);
 
   printf("goodbye\n" );
 }
