4972b8240fc59deb219bc02e9a8751025bd27bfc
Matthew Moreno <mmore500@gmail.com>
2017-03-06 23:23:56 -0800

    Use waitpid instead of wait
    This change prevents wait from being prematurely terminated by
    older children returning.

--- src/path.c
+++ src/path.c
@@ -8,11 +8,11 @@
  * @return 0 on success, 1 on invalid command failure, -1 on other failure
  */
 int exec_path(char *path, char **args, bool wantwait, bool silent) {
-  int f = fork();
+  pid_t fork_val = fork();
 
   // if we don't want to wait, use double forking to prevent zombie processes
   // fork a grandchild and then remove the original child
-  if (!wantwait && !f) {
+  if (!wantwait && !fork_val) {
     // if original child in double forked process, exit immediately
     if(fork()) {
       exit(0);
@@ -20,19 +20,19 @@ int exec_path(char *path, char **args, bool wantwait, bool silent) {
   }
 
   // when using double forking, original parent needs to wait on original child
-  if (!wantwait && f) {
-    wait(NULL);
+  if (!wantwait && fork_val) {
+    waitpid(fork_val, NULL, 0);
   }
 
   // res will be set to 1 if an invalid command was used in wait mode
   int res = 0;
-  if (f > 0 && wantwait){
+  if (fork_val > 0 && wantwait){
     // if parent process and we want to wait on child
     int exitcode = 0;
-    wait(&exitcode);
+    waitpid(fork_val, &exitcode, 0);
     // C's system call multiplies by exit codes by 256
     if (exitcode == COMMAND_NOT_FOUND * 256) res = 1;
-  } else if (f == 0) {
+  } else if (fork_val == 0) {
     // if child`process
     // attempt to run executable
     execv(path, args);
@@ -42,8 +42,8 @@ int exec_path(char *path, char **args, bool wantwait, bool silent) {
     if (!wantwait && !silent) printf("psh: invalid command\n");
     // exit with code "command not found"
     exit(COMMAND_NOT_FOUND);
-  } else if (f < 0) {
-    // if problem forking (f < 0)
+  } else if (fork_val < 0) {
+    // if problem forking (fork_val < 0)
     res = -1;
   }
   return res;
